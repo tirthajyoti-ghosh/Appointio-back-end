897af704ae89255b6359ddf056a4590768b2c4e2
Tirthajyoti Ghosh <itirthahere@gmail.com>
2020-10-02 21:32:38 +0530

    Add User model
    - Add static controller for testing purposes

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 2, maximum: 50}
+  validates :email, presence: true, format: { with: ConstantData::VALID_EMAIL_REGEX }
+    
+end
