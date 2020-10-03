9ab445040669b650f6f10ada85b7c39fa1b0062d
Tirthajyoti Ghosh <itirthahere@gmail.com>
2020-10-04 00:29:27 +0530

    Add registrations controller, add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
 end
