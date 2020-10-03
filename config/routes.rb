d543102fa9f0f9a106136b9666fa06ae2aa8c024
Tirthajyoti Ghosh <itirthahere@gmail.com>
2020-10-04 01:01:47 +0530

    Refactor routes to be more semantic, add logged_in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static#home'
 
-  resources :sessions, only: [:create]
+  post :login, to: "sessions#create"
+  delete :logout, to: "sessions#destroy"
+  get :logged_in, to: "sessions#logged_in"
   resources :registrations, only: [:create]
 end
