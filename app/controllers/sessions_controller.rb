6ff59e2b40f9904e9994799509cd71826e37d7cb
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-04 15:45:18 +0530

    Add logged_in, logout actions in sessions controller
    - Add current_user concern

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController <  ApplicationController
+  include CurrentUserConcern
+
   def create
     user = User
             .find_by(email: params[:user][:email])
@@ -7,13 +9,22 @@ class SessionsController <  ApplicationController
     if user
       session[:user_id] = user.id
 
-      render json: {
-        status: :created,
-        logged_in: true,
-        user: user
-      }
+      render json: { status: :created, logged_in: true, user: user }
     else
       render json: { status: 401 }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: { logged_in: true, user: @current_user }
+    else
+      render json: { logged_in: false }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_in: false }
   end
 end
