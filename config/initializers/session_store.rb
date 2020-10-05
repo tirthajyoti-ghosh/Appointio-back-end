6dc7d5ddeaa6d2e9706b700ad21750bac72cf4f0
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-05 17:02:31 +0530

    Update session_store to work in all env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: "_final_capstone", domain: "http://localhost:3000"
+if Rails.env == "production"
+  # TODO: change the URL when API is hosted on Heroku
+  Rails.application.config.session_store :cookie_store, key: "_final_capstone", domain: "https://tg-final-capstone.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_final_capstone"
+end
