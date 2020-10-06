b2a65ef1a4a87e174bab1ca845b16c36c5cf7c6e
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-06 21:15:05 +0530

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root to: 'static#home'
 
-  post :login, to: "sessions#create"
-  delete :logout, to: "sessions#destroy"
-  get :logged_in, to: "sessions#logged_in"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  get '/logged_in', to: "sessions#logged_in"
+  
   resources :registrations, only: [:create]
+
+  get '/types', to: "types#index"
+
+  get '/apartments', to: "apartments#index"
+
+  resources :appointments, only: [:index, :create, :update, :destroy]
 end
