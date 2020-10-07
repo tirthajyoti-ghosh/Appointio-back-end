e471eea6971d88431d5e6f80638d5f377ac11918
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-07 10:34:13 +0530

    Add index action in apartments controller

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,8 @@
 class ApartmentsController < ApplicationController
+  def index
+    apartments = Apartment.includes(:images, :type)
 
+    # TODO: include images and type
+    render json: { apartments: apartments }
+  end
 end
