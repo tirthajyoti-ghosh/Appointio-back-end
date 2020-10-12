000c26b979812cdd12649d32f18401f55ae92db8
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-12 16:11:29 +0530

    Update cors to allow front-end testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+
+  allow do
     # TODO: change the URL when API is hosted on Heroku
     origins "https://final-capstone-backend.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
