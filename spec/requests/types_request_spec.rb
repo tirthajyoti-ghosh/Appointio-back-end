78852f97f12822476d911bfd4d8c070e1702c6f3
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-12 20:44:42 +0530

    Add types controller tests

--- spec/requests/types_request_spec.rb
+++ spec/requests/types_request_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Apartments", type: :request do
+RSpec.describe "Types", type: :request do
   before :all do
     Type.create(name: 'loft')
+    Type.create(name: 'studio')
+    Type.create(name: 'micro')
 
     Apartment.create(address: '11th Street', rent: '3820', reviews: 4.5, amenities: 'wifi, parking', deposit: '5800', sq_ft: '800', lease_length: '24 months', type_id: Type.first.id)
     Apartment.create(address: '234th Street', rent: '544', reviews: 4.6, amenities: 'wifi, parking, security', deposit: '5300', sq_ft: '300', lease_length: '34 months', type_id: Type.first.id)
@@ -13,19 +15,19 @@ RSpec.describe "Apartments", type: :request do
     Type.destroy_all
   end
 
-  describe 'GET /apartments' do
-    it 'returns all apartments in an array' do
-      get 'http://localhost:3000/apartments'
+  describe 'GET /types' do
+    it 'returns all types in an array' do
+      get 'http://localhost:3000/types'
       
-      expect(JSON[response.body]['apartments'].size).to be(2)
+      expect(JSON[response.body]['types'].size).to be(3)
     end
   end
 
-  describe 'GET /apartments/:id' do
-    it 'returns the single apartment object with the given id' do
-      get "http://localhost:3000/apartments/#{Apartment.last.id}"
-
-      expect(JSON[response.body]['address']).to eq('234th Street')
+  describe 'GET /types/:id' do
+    it 'returns all apartments of the given type(id) in an array' do
+      get "http://localhost:3000/types/#{Type.first.id}"
+      
+      expect(JSON[response.body]['apartments'].size).to be(2)
     end
   end
 end
