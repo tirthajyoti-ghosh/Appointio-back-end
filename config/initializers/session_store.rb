827efd908a20dac758add88e1f5df7c42d4b6dfe
tirthajyoti-ghosh <itirthahere@gmail.com>
2020-10-13 18:11:45 +0530

    Update session_store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,1 @@
-if Rails.env == "production"
-  # TODO: change the URL when API is hosted on Heroku
-  Rails.application.config.session_store :cookie_store, key: "_final_capstone", domain: "https://adoring-pike-d04b95.netlify.app"
-else
-  Rails.application.config.session_store :cookie_store, key: "_final_capstone"
-end
+Rails.application.config.session_store :cookie_store, key: "_final_capstone", :domain => :all
